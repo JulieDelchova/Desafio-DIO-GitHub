e17931e37855c41f518b7eb5de081d9ef22d7fe9
JulieDelchova <juliedelchova@gmail.com>
2022-08-07 10:29:09 -0300

    Inserindo package Calculadora

---
+++ DominandoIDEJava/FamiliarizandoIDE/src/br/com/dio/Calculadora/calculadora.java
@@ -0,0 +1,51 @@
+package br.com.dio.Calculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o valor do primeiro número: ");
+        a = scan.nextInt();
+
+        System.out.println("Digite o valor do segundo número: ");
+        b = scan.nextInt();
+
+
+        int somar = somar(a, b);
+        int subtrair = subtrair(a, b);
+        int multiplicar = multiplicar(a, b);
+        int dividir = dividir(a, b);
+
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
+
+    }
+
+    public static int somar (int a, int b){
+
+       return a + b;
+    }
+
+    public static int subtrair (int a, int b){
+
+       return a - b;
+    }
+
+    public static int multiplicar (int a, int b){
+
+       return a * b;
+    }
+
+    public static int dividir (int a, int b){
+
+       return a / b;
+    }
+}
